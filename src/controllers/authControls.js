f408d62ee43359d80be0a046277529300f1e1d21
habib-prog <xavierjames701@gmail.com>
2026-04-12 02:41:25 +0600

    first commit

---
+++ src/controllers/authControls.js
@@ -0,0 +1,13 @@
+const registration = async (req, res) => {
+  const { username, email, password } = req.body;
+  try {
+  } catch (error) {}
+};
+
+const login = async (req, res) => {
+  const { email, pass } = req.body;
+  try {
+  } catch (error) {}
+};
+
+module.exports = { registration, login };
